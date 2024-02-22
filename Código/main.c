6c7304cb786ba9fbe87104e6ae48c1fcbd4d8874
Jônatas Mascarenhas Lima <60526989+jonataslima555@users.noreply.github.com>
2024-02-22 12:21:59 -0300

    Add files via upload

---
+++ Código/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int i, max, min;
+    int potencia[20];
+
+    printf("Digite 20 valores de potência:\n");
+    for(i = 0; i < 20; i++) {
+        scanf("%d", &potencia[i]);
+    }
+
+    max = min = potencia[0];
+    for(i = 1; i < 20; i++) {
+        if(potencia[i] > max) {
+            max = potencia[i];
+        }
+        if(potencia[i] < min) {
+            min = potencia[i];
+        }
+    }
+
+    printf("O valor máximo atingido foi: %d\n", max);
+    printf("O valor mínimo atingido foi: %d\n", min);
+
+    return 0;
+}
